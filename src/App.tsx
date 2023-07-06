4bd6e24452cb891b04d0f2f157e1337c8a68c306
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-07-06 15:14:17 -0400

    Fixed styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import Home from './pages/home/Home';
 
 function App() {
 	return (
-		<>
-			<h1 className=" text-4xl">Minesweeper</h1>
+		<main className="p-5">
+			<h1 className="text-center font-mono text-4xl font-bold">Minesweeper</h1>
+			<br />
 			<Home />
-		</>
+		</main>
 	);
 }
 
