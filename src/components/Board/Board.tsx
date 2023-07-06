13ed0e232bd057c276761440bbcbaaaf718bed2c
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-07-06 15:14:44 -0400

    Added emoji to display status of the game

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Game } from '../../types/Game';
 import Cell from '../Cell/';
 
@@ -7,16 +8,34 @@ export default function Board({
 	handleCellRightClick,
 	secondsElapsed,
 	flags,
+	isGameOver,
 }: Game) {
+	const [isClicking, setIsClicking] = useState(false);
+
+	const onMouseEvent = () => {
+		setIsClicking((prev) => !prev);
+	};
+
 	return (
 		<div>
 			<div className=" w-full bg-neutral-300 p-5">
 				<div className="flex justify-around font-mono text-2xl font-bold text-red-600">
-					<p className="rounded-sm bg-slate-800 p-2">{flags}</p>
-					<p className="rounded-sm bg-slate-800 p-2">{secondsElapsed}</p>
+					<p className="w-20 rounded-sm bg-slate-800 p-2 text-center">
+						{flags}
+					</p>
+					<p className="rounded-sm bg-slate-800 p-2 text-center">
+						{!isGameOver ? (!isClicking ? '😃' : '😲') : '💀'}
+					</p>
+					<p className="w-20 rounded-sm bg-slate-800 p-2 text-center">
+						{secondsElapsed}
+					</p>
 				</div>
 			</div>
-			<div className="flex flex-col gap-1 bg-neutral-100 p-1">
+			<div
+				className="flex flex-col gap-1 bg-neutral-100 p-1"
+				onMouseDown={onMouseEvent}
+				onMouseUp={onMouseEvent}
+			>
 				{board.map((row, idx) => {
 					return (
 						<div key={idx} className="flex gap-1">
